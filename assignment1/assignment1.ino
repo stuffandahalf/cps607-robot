3fec87502bd5dfe6a6080c3100f18c37ce3e0298
Gregory Norton <gregory.norton@me.com>
2019-09-25 13:40:52 -0400

    Mostly functional

--- assignment1/assignment1.ino
+++ assignment1/assignment1.ino
@@ -1,11 +1,13 @@
+//#define SERIAL_DEBUG      // Uncomment to enable serial output
+//#define USING_INTERRUPTS  // uncomment this to switch to using interrupts, couldn't get transistor AND gate working
+//#define DONT_MOVE
+
 // Motor control pins
 // Must be PWM capable
 #define FB1         (6)
 #define FB2         (5)
 #define LR1         (9)
 #define LR2         (10)
-
-//#define USING_INTERRUPTS  // uncomment this to switch to using interrupts, couldn't get transistor AND gate working
 
 #ifdef USING_INTERRUPTS
 // IR sensors are active low, AND them all
@@ -18,19 +20,12 @@
 #define IR_SENSE_BL (A5)
 #define IR_SENSE_BR (A4)
 
-#define SERIAL_DEBUG
-
-#ifdef SERIAL_DEBUG
-#define PRINT(...) Serial.println(__VA_ARGS__)
-#else
-#define PRINT(...)
-#endif
 
 #define IRSTATUS_CLEAR              (0u)
-#define IRSTATUS_FRONT_RIGHT        (1u)
-#define IRSTATUS_FRONT_LEFT         (2u)
-#define IRSTATUS_BACK_RIGHT         (4u)
-#define IRSTATUS_BACK_LEFT          (8u)
+#define IRSTATUS_FRONT_RIGHT        (1 << 0)
+#define IRSTATUS_FRONT_LEFT         (1 << 1)
+#define IRSTATUS_BACK_RIGHT         (1 << 3)
+#define IRSTATUS_BACK_LEFT          (1 << 4)
 
 #define IRSTATUS_FRONT_EDGE         (IRSTATUS_FRONT_LEFT | IRSTATUS_FRONT_RIGHT)
 #define IRSTATUS_BACK_EDGE          (IRSTATUS_BACK_LEFT | IRSTATUS_BACK_RIGHT)
@@ -44,9 +39,25 @@
 
 #define IRSTATUS_TRAPPED            (15u)
 
-#define TURN_PWM (200)
-#define TURN_DELAY (500)
-#define SPEED_PWM (70)
+#define DIRECTION_NONE              (0u)
+#define DIRECTION_LEFT              (1 << 0)
+#define DIRECTION_RIGHT             (1 << 1)
+#define DIRECTION_FORWARD           (1 << 2)
+#define DIRECTION_REVERSE           (1 << 3)
+#define DIRECTION_UNDEFINED         (1 << 7)
+
+//#define RIGHT_TURN_PWM (185)
+#define RIGHT_TURN_PWM (200)
+#define LEFT_TURN_PWM (200)
+//#define TURN_DELAY (250)
+#define TURN_DELAY (100)
+#define SPEED_PWM (75)
+
+// When running the motors at a low speed, need to start with a higher PWM to avoid stalling
+//#define KICKSTART_PWM   100
+//#define KICKSTART_PWM   150
+#define KICKSTART_PWM   125
+#define KICKSTART_DELAY (255 - KICKSTART_PWM)
 
 class MotorControl
 {
@@ -80,8 +91,8 @@ public:
         this->pwm = pwm & 0xFF;
         
         digitalWrite(this->outB, LOW);
-        analogWrite(this->outA, 100);
-        delay(75);
+        analogWrite(this->outA, KICKSTART_PWM);
+        delay(KICKSTART_DELAY);
         analogWrite(this->outA, pwm & 0xFF);
     }
     
@@ -94,8 +105,8 @@ public:
         }
         this->pwm = (pwm & 0xFF) * -1;
         digitalWrite(this->outA, LOW);
-        analogWrite(this->outB, 100);
-        delay(75);
+        analogWrite(this->outB, KICKSTART_PWM);
+        delay(KICKSTART_DELAY);
         analogWrite(this->outB, pwm & 0xFF);
     }
     
@@ -188,6 +199,7 @@ void printIRStatus(uint8_t status)
 bool interrupted;
 bool handled;
 #endif
+uint8_t lastDirection;
 MotorControl *mcForwardBackward;
 MotorControl *mcLeftRight;
 
@@ -222,9 +234,14 @@ void setup()
     pinMode(IR_INT, INPUT);
     attachInterrupt(digitalPinToInterrupt(IR_INT), IR_ISR, FALLING);
 #endif
-
+    
+    lastDirection = DIRECTION_FORWARD;
     mcLeftRight->brake();
+#ifdef DONT_MOVE
+    mcForwardBackward->brake();
+#else
     mcForwardBackward->forward(SPEED_PWM);
+#endif
 }
 
 // left = forward
@@ -232,6 +249,10 @@ void setup()
 
 void loop()
 {
+#ifdef DONT_MOVE
+    return;
+#endif
+
 #ifdef USING_INTERRUPTS
     if (!interrupted) {
         return;
@@ -240,9 +261,14 @@ void loop()
         return;
     }
 #endif
-    uint16_t ir_status = getIRStatus();
-    printIRStatus(ir_status);
+
+    uint8_t direction = DIRECTION_UNDEFINED;
+    uint16_t ir_status;
     
+#ifdef SERIAL_DEBUG
+    ir_status = getIRStatus();
+    printIRStatus(ir_status);
+#endif
     if ((ir_status = getIRStatus()) != IRSTATUS_CLEAR) {
 #ifndef USING_INTERRUPTS
         //mcForwardBackward->brake();
@@ -251,29 +277,61 @@ void loop()
         // corner cases
         case IRSTATUS_FRONT_LEFT:
             if (mcForwardBackward->getPwm() >= 0) {
-                mcForwardBackward->reverse();
-                mcLeftRight->forward(TURN_PWM);
+                mcForwardBackward->reverse(SPEED_PWM);
+                direction = DIRECTION_REVERSE;
+                if (lastDirection & DIRECTION_LEFT) {
+                    mcLeftRight->reverse(RIGHT_TURN_PWM);
+                    direction |= DIRECTION_RIGHT;
+                }
+                else {
+                    mcLeftRight->forward(LEFT_TURN_PWM);
+                    direction |= DIRECTION_LEFT;
+                }
                 delay(TURN_DELAY);
             }
             break;
         case IRSTATUS_FRONT_RIGHT:
             if (mcForwardBackward->getPwm() >= 0) {
-                mcForwardBackward->reverse();
-                mcLeftRight->reverse(TURN_PWM);
+                mcForwardBackward->reverse(SPEED_PWM);
+                direction = DIRECTION_REVERSE;
+                if (lastDirection & DIRECTION_RIGHT) {
+                    mcLeftRight->forward(LEFT_TURN_PWM);
+                    direction |= DIRECTION_LEFT;
+                }
+                else {
+                    mcLeftRight->reverse(RIGHT_TURN_PWM);
+                    direction |= DIRECTION_RIGHT;
+                }
                 delay(TURN_DELAY);
             }
             break;
         case IRSTATUS_BACK_LEFT:
             if (mcForwardBackward->getPwm() <= 0) {
-                mcForwardBackward->reverse();
-                mcLeftRight->reverse(TURN_PWM);
+                mcForwardBackward->forward(SPEED_PWM);
+                direction = DIRECTION_FORWARD;
+                if (lastDirection & DIRECTION_RIGHT) {
+                    mcLeftRight->forward(LEFT_TURN_PWM);
+                    direction |= DIRECTION_LEFT;
+                }
+                else {
+                    mcLeftRight->reverse(RIGHT_TURN_PWM);
+                    direction |= DIRECTION_RIGHT;
+                }
                 delay(TURN_DELAY);
             }
             break;
         case IRSTATUS_BACK_RIGHT:
             if (mcForwardBackward->getPwm() <= 0) {
-                mcForwardBackward->reverse();
-                mcLeftRight->forward(TURN_PWM);
+                mcForwardBackward->forward(SPEED_PWM);
+                direction = DIRECTION_FORWARD;
+                if (lastDirection & DIRECTION_LEFT) {
+                    mcLeftRight->reverse(RIGHT_TURN_PWM);
+                    direction |= DIRECTION_RIGHT;
+                }
+                else {
+                    mcLeftRight->forward(LEFT_TURN_PWM);
+                    direction |= DIRECTION_LEFT;
+                }
                 delay(TURN_DELAY);
             }
             break;
@@ -281,52 +339,76 @@ void loop()
         // edge cases
         case IRSTATUS_FRONT_EDGE:
             if (mcForwardBackward->getPwm() >= 0) {
-                mcForwardBackward->reverse();
+                mcForwardBackward->reverse(SPEED_PWM);
                 mcLeftRight->brake();
+                direction = DIRECTION_REVERSE;
             }
             break;
         case IRSTATUS_BACK_EDGE:
             if (mcForwardBackward->getPwm() <= 0) {
-                mcForwardBackward->reverse();
+                mcForwardBackward->forward(SPEED_PWM);
                 mcLeftRight->brake();
+                direction = DIRECTION_FORWARD;
             }
             break;
-        case IRSTATUS_LEFT_EDGE:
-            mcLeftRight->reverse(TURN_PWM);
+        case IRSTATUS_LEFT_EDGE:    // swap forwards / reverse?
+            mcLeftRight->reverse(RIGHT_TURN_PWM);
+            direction &= ~(DIRECTION_LEFT | DIRECTION_RIGHT);
+            direction |= DIRECTION_RIGHT;
             delay(TURN_DELAY);
             break;
         case IRSTATUS_RIGHT_EDGE:
-            mcLeftRight->forward(TURN_PWM);
+            mcLeftRight->forward(LEFT_TURN_PWM);
+            direction &= ~(DIRECTION_LEFT | DIRECTION_RIGHT);
+            direction |= DIRECTION_LEFT;
             delay(TURN_DELAY);
             break;
             
-        //// 2 edges
-        //case IRSTATUS_FRONT_LEFT_EDGES:
-            //mcLeftRight->forward(TURN_PWM);
-            //mcForwardBackward->reverse();
-            //break;
-        //case IRSTATUS_FRONT_RIGHT_EDGES:
+        // 2 edges
+        case IRSTATUS_FRONT_LEFT_EDGES: // reverse and go right
+#if 0
+            mcForwardBackward->brake();
+#else
+            mcForwardBackward->reverse(SPEED_PWM);
+            mcLeftRight->reverse(RIGHT_TURN_PWM);
+            delay(TURN_DELAY / 2);
+            mcForwardBackward->reverse();
+#endif
+            break;
+        case IRSTATUS_FRONT_RIGHT_EDGES:
+#if 1
+            mcForwardBackward->brake();
+#else
             //mcLeftRight->reverse(TURN_PWM);
             //mcForwardBackward->reverse();
-            //break;
-        //case IRSTATUS_BACK_LEFT_EDGES:
-            //mcLeftRight->forward(TURN_PWM);
-            //mcForwardBackward->reverse();
-            //break;
-        //case IRSTATUS_BACK_RIGHT_EDGES:
-            //mcLeftRight->reverse(TURN_PWM);
-            //mcForwardBackward->reverse();
-            //break;
+#endif
+            break;
+        case IRSTATUS_BACK_LEFT_EDGES:
+            mcForwardBackward->forward(SPEED_PWM);
+            mcLeftRight->reverse(RIGHT_TURN_PWM);
+            direction = DIRECTION_FORWARD | DIRECTION_RIGHT;
+            delay(TURN_DELAY);
+            break;
+        case IRSTATUS_BACK_RIGHT_EDGES:
+            mcForwardBackward->forward(SPEED_PWM);
+            mcLeftRight->forward(LEFT_TURN_PWM);
+            direction = DIRECTION_FORWARD | DIRECTION_LEFT;
+            delay(TURN_DELAY);
+            break;
             
-        //case IRSTATUS_TRAPPED:
-            //// Trapped, nowhere to move, will resume if relocated
-            //break;
+        case IRSTATUS_TRAPPED:
+            // Trapped, nowhere to move, will resume if relocated
+            break;
         }
         //mcLeftRight->brake();
         //mcForwardBackward->resume();
     }
     else {
         mcLeftRight->brake();
+    }
+    
+    if (direction != DIRECTION_UNDEFINED) {
+        lastDirection = direction;
     }
     
 #ifdef USING_INTERRUPTS
