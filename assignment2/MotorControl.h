22044d7193d3ca0ab346aad6eb4d4b4dac813e24
Gregory Norton <gregory.norton@me.com>
2019-10-04 14:11:24 -0400

    Added new code for assignment 2

---
+++ assignment2/MotorControl.h
@@ -0,0 +1,93 @@
+#ifndef MOTORCONTROL_H
+#define MOTORCONTROL_H
+
+class MotorControl
+{
+private:
+    uint8_t outA;
+    uint8_t outB;
+    
+    int16_t pwm;
+    bool brakeStatus;
+    
+    //uint8_t kickstartPwm;
+    //uint8_t kickstartDelay;
+
+public:
+
+
+    MotorControl(uint8_t outA, uint8_t outB)
+    {
+        //this->kickstartPwm = KICKSTART_PWM;
+        //this->kickstartDelay = KICKSTART_DELAY;
+        
+        this->outA = outA;
+        this->outB = outB;
+        
+        pinMode(this->outA, OUTPUT);
+        pinMode(this->outB, OUTPUT);
+        
+        brake();
+    }
+    
+    /*MotorControl(uint8_t outA, uint8_t outB, uint8_t kickstartPwm, uint8_t kickstartDelay) : MotorControl(outA, outB)
+    {
+        this->kickstartPwm = kickstartPwm;
+        this->kickstartDelay = kickstartDelay;
+    }*/
+    
+    //inline void resume() { this->forward(); }
+    
+    //inline void forward() { this->forward(this->pwm); }
+    inline void forward(int16_t pwm)
+    {
+        if (pwm == 0) {
+            this->brake();
+            return;
+        }
+        if (pwm < 0) {
+            reverse(pwm * -1);
+            return;
+        }
+        this->pwm = pwm & 0xFF;
+        
+        digitalWrite(this->outB, LOW);
+        /*analogWrite(this->outA, KICKSTART_PWM);
+        delay(KICKSTART_DELAY);*/
+        analogWrite(this->outA, pwm & 0xFF);
+        this->brakeStatus = false;
+    }
+    
+    //inline void reverse() { this->forward(this->pwm * -1); }
+    inline void reverse(int16_t pwm)
+    {
+        if (pwm < 0) {
+            forward(pwm * -1);
+            return;
+        }
+        this->pwm = (pwm & 0xFF) * -1;
+        digitalWrite(this->outA, LOW);
+        /*analogWrite(this->outB, KICKSTART_PWM);
+        delay(KICKSTART_DELAY);*/
+        analogWrite(this->outB, pwm & 0xFF);
+        this->brakeStatus = false;
+    }
+    
+    inline void brake()
+    {
+        digitalWrite(this->outA, HIGH);
+        digitalWrite(this->outB, HIGH);
+        this->brakeStatus = true;
+    }
+    
+    inline void standby()
+    {
+        digitalWrite(this->outA, LOW);
+        digitalWrite(this->outB, LOW); 
+    }
+    
+    inline bool getBrakeStatus() { return this->brakeStatus; }
+    inline int16_t getPwm() { return this->pwm; }
+};
+
+#endif
---
+++ assignment2/assignment2.ino
@@ -0,0 +1,106 @@
+#include "MotorControl.h"
+
+// constants for the speed of sound
+#define SPEED_OF_SOUND_MpS 343
+#define SPEED_OF_SOUND_MMpS (SPEED_OF_SOUND_MpS / 1000)
+
+#define MOTOR_LA (10)
+#define MOTOR_LB (9)
+#define MOTOR_RA (6)
+#define MOTOR_RB (5)
+
+#define IR_SENSE_F (A0)
+#define IR_SENSE_R (A1)
+#define IR_SENSE_L (A2)
+
+#define DISTANCE_SENSE_TRIGGER  (7)
+#define DISTANCE_SENSE_ECHO     (8)
+
+#define SENSE_CLEAR     (0u)
+#define SENSE_IR_FRONT  (1 << 0)
+#define SENSE_IR_LEFT   (1 << 2)
+#define SENSE_IR_RIGHT  (1 << 3)
+
+#define SENSE_IR_FRONT_LEFT    (IR_STATUS_FRONT | IR_STATUS_LEFT)
+#define SENSE_IR_FRONT_RIGHT   (IR_STATUS_FRONT | IR_STATUS_RIGHT)
+#define SENSE_IR_OFF_TABLE     (IR_STATUS_FRONT | IR_STATUS_LEFT | IR_STATUS_RIGHT)
+
+/* Macros to change the execution style */
+//#define SERIAL_DEBUG
+//#define DONT_MOVE
+
+#ifdef SERIAL_DEBUG
+#define PRINT_SERIAL(...) Serial.print(__VA_ARGS__);
+#else
+#define PRINT_SERIAL(...)
+#endif
+
+MotorControl *leftMotor;
+MotorControl *rightMotor;
+
+template <typename T>
+inline bool inRange(T x, T a, T b) { return a <= x && x < b; }
+
+uint8_t getIRSensorStatus()
+{
+    uint8_t state = SENSE_CLEAR;
+    if (digitalRead(IR_SENSE_F) {
+        state |= SENSE_IR_FRONT;
+    }
+    if (digitalRead(IR_SENSE_L) {
+        state |= SENSE_IR_LEFT;
+    }
+    if (digitalRead(IR_SENSE_R) {
+        state |= SENSE_IR_RIGHT;
+    }
+    return state;
+}
+
+int16_t getDistance()
+{
+    digitalWrite(DISTANCE_SENSE_TRIGGER, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(DISTANCE_SENSE_TRIGGER, LOW);
+    
+    unsigned long duration = pulseIn(DISTANCE_SENSE_ECHO, HIGH);
+    if (!duration) {
+        return -1;
+    }
+    
+    int16_t distance = duration * SPEED_OF_SOUND_MMpS; // distance is now in millimeters
+    
+    return distance;
+}
+
+void setup()
+{
+#ifdef SERIAL_DEBUG
+    Serial.begin(115200);
+#endif
+    
+    leftMotor = new MotorControl(MOTOR_LA, MOTOR_LB);
+    rightMotor = new MotorControl(MOTOR_RB, MOTOR_RA);
+    
+    pinMode(IR_SENSE_F, INPUT);
+    pinMode(IR_SENSE_R, INPUT);
+    pinMode(IR_SENSE_L, INPUT);
+    
+#ifndef DONT_MOVE
+    leftMotor->forward(255);
+    rightMotor->forward(255);
+#endif
+}
+
+void loop()
+{
+#ifdef DONT_MOVE
+    return;
+#endif
+    
+    
+    PRINT_SERIAL(getDistance());
+    /*uint8_t sensorStatus;
+    if ((sensorStatus = getSensorStatus()) != SENSE_CLEAR) {
+        
+    }*/
+}
